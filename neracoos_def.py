c5de83af024c51a27ab371176c3e9f91458fdd03
Huanxin Xu <xhx509@gmail.com>
2013-05-29 13:42:06 -0400

    for getting neracoos data

--- neracoos_def.py
+++ neracoos_def.py
@@ -27,7 +27,10 @@ def get_neracoos_ctl(inputfilename):# get data input from a txt file
        maxdtime=dt.datetime.strptime(dtime[1],'%Y,%m,%d,%H,%M') 
    else:
        dtime=f.readline()
-       
+       mindtime=dt.datetime.strptime('2001,1,1,0,0','%Y,%m,%d,%H,%M')
+       maxdtime=dt.datetime.strptime('2017,1,1,0,0','%Y,%m,%d,%H,%M') #w
+   model=f.readline() #get mooring model
+   model=model[0:model.index(']')].strip('[')
    if select2 =='1': #get depth_range
        idepth=f.readline()
        idepth=idepth[0:idepth.index(']')].strip('[').split(',')
@@ -37,11 +40,6 @@ def get_neracoos_ctl(inputfilename):# get data input from a txt file
        i_mindepth=0
        i_maxdepth=2000
        dtime=f.readline()
-       
-   model=f.readline() #get mooring model
-   model=model[0:model.index(']')].strip('[')    
-       
-
        
    if select3 =='1': #get sites
        site=f.readline()
@@ -82,6 +80,7 @@ def get_neracoos_ctl_id_max(url):  #get the max datetime , min datetime and inde
         for i in list(dtime['time']):
             i=round(i,7)
             ddd.append(i)
+            
         #print "This option has data from "+str(num2date(dd[0]+date2num(datetime.datetime(2001, 1, 1, 0, 0))))+" to "+str(num2date(dd[-1]+date2num(datetime.datetime(2001, 1, 1, 0, 0)))) 
         #print 'This option has data from '+num2date(dd[0]).strftime("%B %d, %Y")+' to '+num2date(dd[-1]).strftime("%B %d, %Y")  
                 
@@ -106,9 +105,9 @@ def get_id_s_id_e_id_max_url(url,sdtime_n,edtime_n): # get id of start time and 
             id_s=get_neracoos_ctl_id(url,sdtime_n)
             if mintime<=edtime_n<=maxtime:
                 id_e=get_neracoos_ctl_id(url,edtime_n)
-            if edtime>maxtime:
+            if edtime_n>maxtime:
                 id_e=id_max_url
-        if mintime>maxtime:
+        if sdtime_n>maxtime:
             id_s=''
             id_e=''
         
@@ -165,13 +164,13 @@ def depth_select(sites,i_mindepth,i_maxdepth): #select depth which we have in we
             print sites[k]+' is not here,please check  your input '
         '''
     return depths,site_d
-def get_neracoos_data(url,id_s,id_e,id_max_url): #get data from neracoos.
+def get_neracoos_temp_data(url,id_s,id_e,id_max_url): #get temperature data from neracoos.
           url=url+'temperature[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0]'
           database=open_url(url)['temperature'][int(id_s):int(id_e)]
-          lat=database['lat']
-          lat=round(lat[0],2)
-          lon=database['lon']
-          lon=round(lon[0],2)
+          #lat=database['lat']
+          #lat=round(lat[0],2)
+          #lon=database['lon']
+          #lon=round(lon[0],2)
           depth=database['depth']
           period=database['time']
           temp=database['temperature']
@@ -181,7 +180,112 @@ def get_neracoos_data(url,id_s,id_e,id_max_url): #get data from neracoos.
     
           for i in range(len(period)): #convert format to list
              period_str.append(dt.datetime.strftime(period[i],'%Y-%m-%d-%H-%M'))
-             #temp_list.append(temp[i][0][0][0])
-             #depth_list.append(int(depth))  
-             depth_temp.append([round(depth[0],1),round(temp[i][0][0][0],2),lat,lon])
-          return period_str,depth_temp
+           
+             depth_temp.append([round(depth[0],1),round(temp[i][0][0][0],2)])
+          temp1=[] #get rid of bad data
+          for i in range(len(depth_temp)):
+            temp1.append(depth_temp[i][1])
+      
+          id_bad=ml.find((np.array(temp1)>30) | (np.array(temp1)<-4))
+          #print id_bad
+          id_bad=list(id_bad)
+          id_bad.reverse()
+          for m in id_bad:
+             del period_str[m]
+             del depth_temp[m]
+          for i in range(len(depth_temp)):
+              if i>=len(depth_temp):
+                  break
+              if abs(depth_temp[i-1][1]-depth_temp[i][1])>2:
+                  del depth_temp[i]
+                  del period_str[i]
+                  depth_temp=depth_temp    
+                  period_str=period_str
+                  i=i-1
+          #df=DataFrame(np.array(depth_temp),index=period_str,columns=['depth','temp','lat','lon'])
+          return depth_temp,period_str
+def get_neracoos_wind_data(url,id_s,id_e,id_max_url): #get wind data from neracoos.
+         url1=url+'wind_speed[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0],wind_direction[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0]'
+         database_s=open_url(url1)['wind_speed'][int(id_s):int(id_e)]
+         database_d=open_url(url1)['wind_direction'][int(id_s):int(id_e)]
+         #lat=database_s['lat']
+         #lat=round(lat[0],2)
+         #lon=database_s['lon']
+         #lon=round(lon[0],2)
+         depth=database_s['wind_depth']
+         period=database_s['time']
+         speed=database_s['wind_speed']
+         speed=speed[0:].tolist()
+         period=num2date(period[0:]+date2num(dt.datetime(1858, 11, 17, 0, 0)))
+         direction=database_d['wind_direction']
+         direction=direction[0:].tolist()
+         period_str,wind_all=[],[]
+         for i in range(len(period)): #convert format to list
+             period_str.append(dt.datetime.strftime(period[i],'%Y-%m-%d-%H-%M'))
+             wind_all.append([round(depth[0],1),round(speed[i][0][0][0],2),round(direction[i][0][0][0],2)])
+         wind,direction=[],[] # figure out bad data and delete
+         for i in range(len(wind_all)):
+           wind.append(wind_all[i][1])
+           direction.append(wind_all[i][2])
+         id_bad=ml.find((np.array(wind)>300) | (np.array(wind)<-1) | (np.array(direction)<0)| (np.array(direction)>360))
+         #print id_bad
+         id_bad=list(id_bad)
+         id_bad.reverse()
+         for m in id_bad:
+            del period_str[m]
+            del wind_all[m]
+         return period_str,wind_all
+def get_neracoos_current_data(url,id_s,id_e,id_max_url): #get wind data from neracoos.
+         url1=url+'current_speed[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0],current_direction[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0],current_u[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0],current_v[0:1:'+id_max_url+'][0:1:0][0:1:0][0:1:0]'
+         database_s=open_url(url1)['current_speed'][int(id_s):int(id_e)] 
+         database_d=open_url(url1)['current_direction'][int(id_s):int(id_e)]
+         database_u=open_url(url1)['current_u'][int(id_s):int(id_e)]
+         database_v=open_url(url1)['current_v'][int(id_s):int(id_e)]
+         #lat=database_s['lat']
+         #lat=round(lat[0],2)
+         #lon=database_s['lon']
+         #lon=round(lon[0],2)
+         
+         period=database_s['time']
+         speed=database_s['current_speed']
+         speed=speed[0:].tolist()
+         period=num2date(period[0:]+date2num(dt.datetime(1858, 11, 17, 0, 0)))
+         direction=database_d['current_direction']
+         direction=direction[0:].tolist()
+         u=database_u['current_u']
+         u=u[0:].tolist()    
+         v=database_v['current_v']
+         v=v[0:].tolist() 
+         period_str,current_all=[],[]
+         for i in range(len(period)): #convert format to list
+             period_str.append(dt.datetime.strftime(period[i],'%Y-%m-%d-%H-%M'))
+             current_all.append([round(speed[i][0][0][0],2),round(direction[i][0][0][0],2),round(u[i][0][0][0],2),round(v[i][0][0][0],2)])
+         current,u,v,direction=[],[],[],[]# figure out bad data and delete
+         for i in range(len(current_all)):
+             current.append(current_all[i][0])
+             direction.append(current_all[i][1])
+             u.append(current_all[i][2])
+             v.append(current_all[i][3])
+         id_bad=ml.find((np.array(current)>200) | (np.array(current)<-1)|(np.array(direction)<0)| (np.array(direction)>360)|(np.array(u)<-200)| (np.array(u)>200)|(np.array(v)<-200)| (np.array(v)>200))
+         #print id_bad
+         id_bad=list(id_bad)
+         id_bad.reverse()
+         for m in id_bad:
+            del period_str[m]
+            del current_all[m]         
+         return period_str,current_all
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
